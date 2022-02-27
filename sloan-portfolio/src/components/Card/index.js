2d94972b825bac31ce5dd07c71560a31243449f9
Sloan <Sloan.boyce@gmail.com>
2022-02-26 22:40:26 -0500

    added card to project component

---
+++ sloan-portfolio/src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Card(props) {
+
+    return (
+        <div className='card'>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </div>
+    );
+}
+
+export default Card;
--- sloan-portfolio/src/components/Project/index.js
+++ sloan-portfolio/src/components/Project/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../Card';
 
 function Project() {
+
+    const [projectList] = useState([
+        {
+            title: "Weeb Keepers",
+            description: "A anime tracking application where users can browse, track and keep updated with their anime watchlist."
+        },
+        {
+            title: "Critique Code",
+            description: "A place where users can gather to discuss all things code, get advice, post funny code etc."
+        }
+    ]);
+
     return (
-        <h1>Project</h1>
+       <div>
+           <h2>Projects</h2>
+           <div className='cards'>
+               {projectList.map(project => (
+                   <Card title={project.title} description={project.description} />
+               ))}
+           </div>
+       </div>
     );
 }
 
